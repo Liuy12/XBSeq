c36a8b3a86313404cad743a0901ba3cedcab614a
7/31/2015 Yuanhang <LiuY12@livemail.uthscsa.edu>
2017-07-21 15:49:12 +0000

    fix test issue
    fit test that issue and update version number
    
    From: Yuanhang Liu <yuanhangliu.ok@gmail.com>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/XBSeq@131482 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/testthat/test_XBplot.R
+++ tests/testthat/test_XBplot.R
@@ -3,8 +3,6 @@ conditions <- factor(c(rep('C',3), rep('T', 3)))
 XB <- XBSeqDataSet(Observed, Background, conditions)
 expect_error(XBplot(XB))
 
-expect_error(XBplot(XB, Samplenum = 'Sample_54_WT'))
-
 expect_error(XBplot(XB, Samplenum = 7))
 
 expect_warning(XBplot(XB, Samplenum = 1, unit = 'LogTPM', Genelength = genelength[,2]))
